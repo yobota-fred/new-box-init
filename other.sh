f4a910b30ee4f06035cda14c6b2df35293f568ce
Fred Collman <fred@yobota.xyz>
2019-04-08 17:39:24 +0100

    Update install instructions for 2019

--- other.sh
+++ other.sh
@@ -1,6 +1,6 @@
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-dpkg -i gitkraken-amd64.deb
-apt install libcurl3
+# wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+# dpkg -i gitkraken-amd64.deb
+# apt install libcurl3
 
 pip install virtualenv
 pip install virtualenvwrapper
@@ -9,19 +9,26 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt install google-chrome-stable
 
-wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
-dkpg -i dropbox_2015.10.28_amd64.deb
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+apt install sublime-text
+
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+apt install pgadmin4
+
+# wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
+# dkpg -i dropbox_2015.10.28_amd64.deb
 
 wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | sh
-nvm install 7.8.0
-nvm use 7.8.0
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install --lts
+nvm use --lts
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+apt update
 apt install yarn
-
-wget https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v1.6/pip/pgadmin4-1.6-py2.py3-none-any.whl
-mkvirtualenv pgadmin4
-workon pgadmin4
-pip install pgadmin4-1.6-py2.py3-none-any.whl
